1a71f67b035f7cb2be517bfe84f6c2167fb5a954
jjankowski <jerzy.jan.jankowski@gmail.com>
2021-08-07 13:10:03 +0200

    second way of quick adding book (from book lists tables)

--- src/app/book-edit/quick-book-input-parser.service.ts
+++ src/app/book-edit/quick-book-input-parser.service.ts
@@ -10,6 +10,7 @@ export class QuickBookInputParserService {
 
   public parseAndUpdateBookWithRaw(bookToUpdate: Book, raw: string): void {
     const rawParts = raw.split('|');
+    const lines = raw.split('\n');
     if (rawParts.length === 13 && rawParts[0] === 'all:::ebook' && rawParts[12] === ':::') {
       bookToUpdate.media = 'ebook';
       bookToUpdate.title = rawParts[1];
@@ -23,6 +24,16 @@ export class QuickBookInputParserService {
       bookToUpdate.subcategory = rawParts[8];
       bookToUpdate.tags = rawParts[9].split(',');
       bookToUpdate.sources = [{name: rawParts[10], location: rawParts[11]}];
+    } else if (lines.length === 7) {
+      bookToUpdate.media = 'ebook';
+      bookToUpdate.title = lines[0];
+      bookToUpdate.rootTitle = lines[0];
+      bookToUpdate.authors = lines[1];
+      bookToUpdate.edition = 1;
+      bookToUpdate.publisher = lines[4];
+      bookToUpdate.publishedYear = +lines[2];
+      bookToUpdate.pagesNumber = +lines[3];
+      bookToUpdate.sources = [{name: lines[5], location: ''}];
     } else {
       bookToUpdate.description = raw;
     }
